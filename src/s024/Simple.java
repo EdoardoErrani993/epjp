acfe73bacc0bf42f87bf6286c83419e8e63584f7
edoardo.errani93@gmail.com <edoardo.errani93@gmail.com>
2019-09-16 18:00:03 +0200

    committo

--- src/s024/Simple.java
+++ src/s024/Simple.java
@@ -9,11 +9,12 @@ import java.lang.Math; // not required
  * @author manny
  */
 public class Simple {
-    public static void main(String[] args) {
-        System.out.println(Math.PI);
-    }
-}
+    public static void main(String[] args) { //metodo main ci sono le tonde che rappresentano un metodo; le quadre rappresentano un array
+        System.out.println(Math.PI);      //print line. Il ; rappresenta la fine dello statement
+    }  //inizia e finisce un blocco
+} //fine classe simple
 
 class PackageClass {
     // TBD
 }
+

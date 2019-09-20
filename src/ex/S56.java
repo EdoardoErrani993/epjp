7dd27f80167db12010be8e78eaa592f8818d9a7d
edoardo.errani93@gmail.com <edoardo.errani93@gmail.com>
2019-09-20 14:42:53 +0200

    20sep - 14.42

--- src/ex/S56.java
+++ src/ex/S56.java
@@ -1,112 +1,112 @@
 package ex;
 
 public class S56 {
-    /**
-     * Reverse a string
-     * 
-     * @param s
-     * @return the input reversed
-     */
-    public static String reverse(String s) {
-    	StringBuilder res = new StringBuilder(s);
-    	res.reverse();
-    	
-        return res.toString();
-        
-    }
+	/**
+	 * Reverse a string
+	 * 
+	 * @param s
+	 * @return the input reversed
+	 */
+	public static String reverse(String s) {
+		StringBuilder res = new StringBuilder(s);
+		res.reverse();
+		return res.toString();
+	}
 
-    /**
-     * Check if the parameter is a palindrome
-     * 
-     * @param s
-     * @return true if the parameter is a palindrome
-     */
-    public static boolean isPalindrome(String s) {
-    	StringBuilder res = new StringBuilder(s);
-    	res.reverse();
-    	if (s.contentEquals(res.toString())) {
-    		return true;
-    	}else {
-        return false;
-    }
-    }
-    /**
-     * Remove vowels from the parameter
-     * 
-     * @param s
-     * @return a string, same of input but without vowels
-     */
-    public static String removeVowels(String s) {
-    	StringBuilder res = new StringBuilder();
-    
-    for (int i = 0; i < s.length(); i++)	{
-    	
-    	if (s.charAt(i) == 'A' ||s.charAt(i) == 'E' ||s.charAt(i) =='I' ||s.charAt(i) =='O' ||s.charAt(i) =='U' ||s.charAt(i) == 'a' ||s.charAt(i) =='e' ||s.charAt(i) =='i' ||s.charAt(i) =='o' ||s.charAt(i) =='u') {
-    		
-    	}else {
-    		res.append(s.charAt(i));
-    	}
-    }
-    	return res.toString();
-    }
+	/**
+	 * Check if the parameter is a palindrome
+	 * 
+	 * @param s
+	 * @return true if the parameter is a palindrome
+	 */
+	public static boolean isPalindrome(String s) {
+		StringBuilder res = new StringBuilder(s);
+		res.reverse();
+		if (s.contentEquals(res.toString())) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-    /**
-     * Convert from binary to decimal
-     * 
-     * @param s a string that contains the binary representation of an integer
-     * @return the converted integer
-     */
-    public static int bin2dec(String s) {
-        
-        return 0;
-    }
+	/**
+	 * Remove vowels from the parameter
+	 * 
+	 * @param s
+	 * @return a string, same of input but without vowels
+	 */
+	public static String removeVowels(String s) {
+		StringBuilder res = new StringBuilder();
 
-    /**
-     * Reverse an array of integers
-     * 
-     * @param data
-     * @return a new array holding the same elements of input, in reversed order
-     */
-    public static int[] reverse(int[] data) {
-        int[] result = new int[data.length];
-        
-        for (int i = data.length - 1; i >=0; i--) {
-        	
-        	result[i]=data[data.length-1-i]; 
-        }
-        
+		for (int i = 0; i < s.length(); i++) {
 
-        return result;
-    }
+			if (s.charAt(i) == 'A' || s.charAt(i) == 'E' || s.charAt(i) == 'I' || s.charAt(i) == 'O'
+					|| s.charAt(i) == 'U' || s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i'
+					|| s.charAt(i) == 'o' || s.charAt(i) == 'u') {
 
-    /**
-     * Calculate the average
-     * 
-     * @param data
-     * @return the average
-     */
-    public static double average(int[] data) {
-    	double result = 0;
-    			
-    	for (int i = 0;i < data.length; i++) {
-    		
-    		result = data [i] + result;
-    		
-    	}
-    	
-    	result = result / data.length;
-    	
-        return result;
-    }
-    
-    /**
-     * Find the largest value
-     * 
-     * @param data
-     * @return the largest value
-     */
-    public static int max(int[] data) {
-       
-        return 0;
-    }
+			} else {
+				res.append(s.charAt(i));
+			}
+		}
+		return res.toString();
+	}
+
+	/**
+	 * Convert from binary to decimal
+	 * 
+	 * @param s a string that contains the binary representation of an integer
+	 * @return the converted integer
+	 */
+	public static int bin2dec(String s) {
+
+		return 0;
+	}
+
+	/**
+	 * Reverse an array of integers
+	 * 
+	 * @param data
+	 * @return a new array holding the same elements of input, in reversed order
+	 */
+	public static int[] reverse(int[] data) {
+		int[] result = new int[data.length];
+
+		for (int i = data.length - 1; i >= 0; i--) {
+
+			result[i] = data[data.length - 1 - i];
+		}
+
+		return result;
+	}
+
+	/**
+	 * Calculate the average
+	 * 
+	 * @param data
+	 * @return the average
+	 */
+	public static double average(int[] data) {
+		double result = 0;
+
+		for (int i = 0; i < data.length; i++) {
+
+			result = data[i] + result;
+
+		}
+
+		result = result / data.length;
+
+		return result;
+	}
+
+	/**
+	 * Find the largest value
+	 * 
+	 * @param data
+	 * @return the largest value
+	 */
+	public static int max(int[] data) {
+
+		return 0;
+	}
 }

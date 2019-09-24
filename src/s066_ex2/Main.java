9542ec822cb08af29d0befcf8437e34056dd022f
Administrator <Administrator@192.168.20.55>
2019-09-24 17:03:08 +0200

    24 sep 17.03

--- src/s066_ex2/Main.java
+++ src/s066_ex2/Main.java
@@ -4,4 +4,5 @@ public class Main {
 	Orco orco = new Orco ("Azog", "Uruk Hai", 100, 30, 10);
 	Umano umano = new Umano ("Aragorn", "Ala compagnia dell'anello", 70, 40, 10);
 	
+	
 }
--- src/s066_ex2/Orco.java
+++ src/s066_ex2/Orco.java
@@ -2,6 +2,6 @@ package s066_ex2;
 
 public class Orco extends Personaggi {
 	public Orco(String Nome, String Clan, int Pv, int Pa, int Bonus) {
-		super(Nome, Clan, Pv, Pa, Bonus); 
-}
+		super(Nome, Clan, Pv, Pa, Bonus);
+	}
 }

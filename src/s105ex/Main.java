1dbc490c5b2429ba6553fa41cbfaa0b13d3e4837
Administrator <Administrator@192.168.20.55>
2019-09-25 17:49:57 +0200

    25 sep 17.49

--- src/s105ex/Main.java
+++ src/s105ex/Main.java
@@ -1,12 +1,19 @@
 package s105ex;
 
 public class Main {
-	
-	//public static void main(String[] args) {
+
+	public static void main(String[] args) {
+
+		MyList mylist = new MyList();
+		for (int i =0; i < 100;i++) {
+			mylist.add(i);
+
+		}
 		
-		//SMyList mylist = new MyList();
 		
-		
-	//}
+		System.out.println(mylist.getSize());
+		System.out.println(mylist.get(89));
+
+	}
 
 }
--- src/s105ex/MyList.java
+++ src/s105ex/MyList.java
@@ -1,28 +1,69 @@
 package s105ex;
 
-
 public class MyList {
-	private Node head; //sto creando un reference
+	private Node head; // sto creando un reference
+	private int sizeValue = 0;
 
 	public void add(int value) {
 		Node node = new Node(value);
-		
+		sizeValue++;
+
 		if (head == null) {
 			head = node;
+			return;
 		}
-		
-		
-	/*	
+
 		Node cur = head;
 		Node nextNode = cur.getNext();
 		while (nextNode != null) {
 			cur = nextNode;
 			nextNode = cur.getNext();
 		}
-		cur.setNext(node);*/
+		cur.setNext(node);
 	}
-/*public int get() {
-	return head;
-}*/
+
+	public int get(int pos) {
+		if (this.head == null) {
+			return 0;
+		}
+
+		if (this.head != null && pos > this.getSize()) {
+			return 0;
+		}
+
+		int index = 0;
+		while (index <= pos) {
+			Node cur = head;
+			Node nextNode = cur.getNext();
+			while (nextNode != null) {
+				cur = nextNode;
+				nextNode = cur.getNext();
+				index++;
+			}
+		}
+		index++;
+
+		return index;
+	}
+
+	public int getSize() { // quanti nodi ho creato
+//
+//		if (head == null) {
+//			return 0;
+//		}
+//		int count = 0;
+//
+//		Node cur = head;
+//		Node nextNode = cur.getNext();
+//		while (nextNode != null) {
+//			cur = nextNode;
+//			nextNode = cur.getNext();
+//			count++;
+//		}
+//		count++;
+
+//		return count;
+		return sizeValue;
+	}
 
 }
